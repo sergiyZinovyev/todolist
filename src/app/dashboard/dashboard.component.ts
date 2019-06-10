21cde2dfbd07ca17cc824cbc677f606d757943ef
sergiyZinovyev <thrion1@gmail.com>
2019-06-10 19:04:55 +0300

    edit dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,16 +10,44 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class DashboardComponent implements OnInit {
 
+newTodoList: any;
+
   constructor(
     private auth: AuthService,
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    let userDoc = this.afs.doc('users/user');
+    userDoc.collection('todolist').valueChanges().subscribe(todoList => {
+      this.newTodoList = todoList;
+    });
   }
+
+  myForm = this.fb.group({
+    //id: ['', [Validators.required]],
+    name: ['', [Validators.required]]
+  })
+
 
   addList() {
-    this.afs.doc('user/1').set({id: 12, name: 'bob'});
+    console.log(this.myForm.value);
+    console.log(this.myForm.controls['name'].value);
+    let taskName = this.myForm.controls['name'].value;
+    this.afs.doc('users/user/todolist/'+taskName).set(this.myForm.value);
   }
 
+  delList(taskName) {
+    this.afs.doc('users/user/todolist/'+taskName).delete();
+  }
+
+  getList(){
+    console.log('test');
+    let userDoc = this.afs.doc('users/user');
+    userDoc.collection('todolist').valueChanges().subscribe(todoList => {
+      this.newTodoList = todoList;
+    });
+    console.log(this.newTodoList)
+  }
 }
